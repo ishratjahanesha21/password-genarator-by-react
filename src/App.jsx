af4e5ab02156d83e92a834eb85a1ebd1e6eb76d5
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2024-08-28 19:17:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Passwordgenaretor from './Components/Passwordgenaretor'
+
+function App() {
+
+
+  return (
+    <>
+      Password genaretors
+      <Passwordgenaretor/>
+    </>
+  )
+}
+
+export default App
